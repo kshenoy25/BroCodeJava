3094786ed04249c332b445933ff737ab366519b6
kshenoy25 <54074703+kshenoy25@users.noreply.github.com>
2024-07-19 08:16:15 -0500

    BroCode youtube java concepts

---
+++ BroCodeYT/src/arrayExample.java
@@ -0,0 +1,17 @@
+public class arrayExample {
+    public static void main(String[] args) {
+    // array = used to store multiple values in a single variable
+
+    String[] carsGiven = new String[3];
+    carsGiven[0] = "Camaro";
+    carsGiven[1] = "Toyota";
+    carsGiven[2] = "Ferrari";
+
+    for (int i = 0; i < carsGiven.length; i++){
+        System.out.println(carsGiven[i]);
+    }
+    }
+
+
+
+}
