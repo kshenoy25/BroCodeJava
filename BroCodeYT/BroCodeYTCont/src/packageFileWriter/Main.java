f0735a0013284ee6dff94a198a091f2b3d646108
kshenoy25 <54074703+kshenoy25@users.noreply.github.com>
2024-07-25 12:33:19 -0500

    BroCode YT cont.

---
+++ BroCodeYT/BroCodeYTCont/src/packageFileWriter/Main.java
@@ -0,0 +1,4 @@
+package packageFileWriter;
+
+public class Main {
+}
--- BroCodeYT/BroCodeYTCont/src/packageHowToWriteFile/Main.java
+++ BroCodeYT/BroCodeYTCont/src/packageHowToWriteFile/Main.java
@@ -1,4 +1,24 @@
 package packageHowToWriteFile;
 
+import java.io.File;
+
 public class Main {
+    public static void main(String[] args) {
+        // file = an abstract representation of file and directory pathnames
+        // if you want to access file in project you follow as shown
+        // if file is located in another area on your computer you find it via the pathway
+        File file = new File("secret_message.txt");
+
+        if(file.exists()){
+            System.out.println("That file exists!");
+            System.out.println(file.getPath());
+            System.out.println(file.getAbsolutePath());
+            System.out.println(file.isFile());
+            file.delete();
+        }
+        else{
+            System.out.println("Oopsie no file exists. Womp Womp.");
+        }
+
+    }
 }
